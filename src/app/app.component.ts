0d51aefadb840762f343b69dfe536d1c4aeb3dad
Žan Sovič <soviczan7@gmail.com>
2025-10-08 11:23:57 +0200

    added items component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { NavbarComponent } from './navbar/navbar.component';
+import { ItemsComponent } from './items/items.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ItemsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
