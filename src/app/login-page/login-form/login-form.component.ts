f238423514f7496696a757098f9b80974462df9a
spiel_meister <soviczan7@gmail.com>
2025-12-15 15:42:43 +0100

    login kinda works?

--- src/app/login-page/login-form/login-form.component.ts
+++ src/app/login-page/login-form/login-form.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { FormInputComponent } from "../../form-elements/form-input/form-input.component";
+import { Component, inject, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [FormInputComponent],
+  imports: [],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
 
+  @Inject(HttpClient) http: HttpClient = inject(HttpClient);
+
+  submit() {
+    const email_ele: HTMLInputElement = document.querySelector("#email")!;
+    const password_ele: HTMLInputElement = document.querySelector("#password")!;
+
+    const email = email_ele.value;
+    const password = password_ele.value;
+
+    const api = new ApiService(this.http);
+    api.login(email, password);
+  }
 }
