5d7bbbfcc97cabad39e711a31658a6f05243c81a
spiel_meister <soviczan7@gmail.com>
2025-12-15 15:51:52 +0100

    fixed signup from

--- src/app/signup-page/signup-form/signup-form.component.ts
+++ src/app/signup-page/signup-form/signup-form.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { FormInputComponent } from '../../form-elements/form-input/form-input.component';
+import { Component, Inject, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [FormInputComponent],
+  imports: [],
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.scss'
 })
 export class SignupFormComponent {
+  @Inject(HttpClient) http: HttpClient = inject(HttpClient);
 
+  private getValueFromId(idSelector: string): string {
+    return (document.querySelector(idSelector)! as HTMLInputElement).value;
+  }
+
+  submit() {
+    const name = this.getValueFromId("#name");
+    const surname = this.getValueFromId("#surname");
+    const email = this.getValueFromId("#email");
+    const password = this.getValueFromId("#password");
+    const passwordRepeat = this.getValueFromId("#password-repeat");
+
+    console.log({
+      name,
+      surname,
+      email,
+      password,
+      passwordRepeat,
+    })
+
+    const api = new ApiService(this.http);
+  }
 }
