efee54849f577bc14b0780bf12d9e1ba27094e40
Žan Sovič <soviczan7@gmail.com>
2025-12-16 18:52:46 +0100

    connected to backend!

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,48 +6,38 @@ import { catchError, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private url: string = "https://localhost:6969";
+  private url: string = "http://localhost:6969";
   constructor(private http: HttpClient) {}
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error(`Something bad happened; please try again later.`));
-  }
 
   login(email: string, password: string) {
     this.http.get(`${this.url}/api/login`, {
+      responseType: "arraybuffer",
       params: {
         email,
         password,
       },
-    }).pipe(
-      catchError(this.handleError),
-    ).subscribe((v) => {
+    }).subscribe((v) => {
         console.log(v);
       });
   }
 
   signup(name: string, surname: string, email: string, password: string) {
-    this.http.get(`${this.url}/api/login`, {
+    this.http.get(`${this.url}/api/signup`, {
+      responseType: "blob",
       params: {
         name,
         surname,
         email,
         password,
       },
-    }).pipe(
-      catchError(this.handleError),
-    ).subscribe((v) => {
-        console.log(v);
+    }).subscribe((_) => {
+        window.location.href = "/login";
       });
+  }
+
+  logout(): void {
+    this.http.get(`${this.url}/api/logout`).subscribe(_ => {
+      window.location.href = "/";
+    });
   }
 }
