3cf4d4b8af65bcf0c2c9000fe7ca8bbf1fb29f61
Žan Sovič <soviczan7@gmail.com>
2025-12-16 18:46:12 +0100

    fixed login form

--- src/app/login-page/login-form/login-form.component.ts
+++ src/app/login-page/login-form/login-form.component.ts
@@ -1,24 +1,32 @@
 import { Component, inject, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
-
   @Inject(HttpClient) http: HttpClient = inject(HttpClient);
+
+  emailMissing: boolean = false;
+  passwordMissing: boolean = false;
 
   submit() {
     const email_ele: HTMLInputElement = document.querySelector("#email")!;
     const password_ele: HTMLInputElement = document.querySelector("#password")!;
 
     const email = email_ele.value;
+    this.emailMissing = email == "";
+
     const password = password_ele.value;
+    this.passwordMissing = email == "";
+
+    if (this.emailMissing || this.passwordMissing) return;
 
     const api = new ApiService(this.http);
     api.login(email, password);
