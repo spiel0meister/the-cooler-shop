6f89e77a7e7817c1959261997fd6c5cd704cb01c
spiel_meister <soviczan7@gmail.com>
2026-03-24 11:21:26 +0100

    added electron, part 2

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Error Handling
+process.on('uncaughtException', (error) => {
+    console.error("Unexpected error: ", error);
+});
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+        }
+    });
+    win.loadURL('http://localhost:4200');
+}
+// App Lifecycle
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
